e7f90f8f99cb47c1ad0c59284d74ea174b7ae419
revarsakpal <117507156+revarsakpal@users.noreply.github.com>
2022-11-23 11:07:00 +0530

    Create Menu.java

---
+++ StudentClassWithMenu/Menu.java
@@ -0,0 +1,15 @@
+
+public class Menu {
+
+	public static void displayMenu() {
+		System.out.println("\n");
+		System.out.println("Select your choice");	
+	 	System.out.println("*******MENU*******");
+	 	System.out.println("1:Add New Studnet");
+	 	System.out.println("2:Find Percentage & GPA of Studnet");	
+	 	System.out.println("3:Display Student by Id");
+	 	System.out.println("4:Display All Students");
+	 	System.out.println("5:EXIT");	
+	}
+
+}
